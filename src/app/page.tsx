c86d984f547f2d471d4d65c3910a7d0d4c22afea
wigor <wigor21@hotmail.com>
2024-09-14 09:09:43 -0300

    feat: redirect root page to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Logo } from "@/components/ui/logo";
+import { redirect } from "next/navigation";
+
 export default function Page() {
-  return <div>...</div>;
+  redirect("/home");
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Logo size={80} />
+    </div>
+  );
 }
