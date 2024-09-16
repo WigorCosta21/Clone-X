f48c34057629e76c4ea2a0c6ccba929b1153b659
wigor <wigor21@hotmail.com>
2024-09-16 11:21:24 -0300

    feat: created profile feed

--- src/app/(ui)/[slung]/page.tsx
+++ src/app/(ui)/[slung]/page.tsx
@@ -1,3 +1,4 @@
+import { ProfileFeed } from "@/components/profile/profile-feed";
 import { Button } from "@/components/ui/button";
 import { GeneralHeader } from "@/components/ui/general-header";
 import { user } from "@/data/user";
@@ -57,6 +58,7 @@ export default function Page() {
           </div>
         </div>
       </section>
+      <ProfileFeed />
     </div>
   );
 }
